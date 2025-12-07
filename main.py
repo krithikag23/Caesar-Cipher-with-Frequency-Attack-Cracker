e615392babba7c83265d5d63e60964eb9c908594
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-12-07 23:28:36 +0530

    update

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # main.py
 from cipher import encrypt, decrypt
 from attack import crack
+
+if __name__ == "__main__":
+    text = "security is fun and cryptography is art"
+    key = 7
+
+    print("Original:", text)
+    enc = encrypt(text, key)
+    print("Encrypted:", enc)
+
+    guessed_key, plaintext = crack(enc)
+    print("\n🔐 Auto-Crack Results")
+    print("Guessed Key:", guessed_key)
+    print("Decrypted:", plaintext)
