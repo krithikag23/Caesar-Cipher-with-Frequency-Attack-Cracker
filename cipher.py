f3923d9dbd7eac6ff0f194bdf8f32dbd0c29e67b
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-12-07 23:26:15 +0530

    update

--- cipher.py
+++ cipher.py
@@ -1,3 +1,12 @@
 # cipher.py
 
 ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+
+def encrypt(text, key):
+    res = ""
+    for ch in text.lower():
+        if ch in ALPHABET:
+            res += ALPHABET[(ALPHABET.index(ch) + key) % 26]
+        else:
+            res += ch
+    return res
